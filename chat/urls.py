7a6bbd499cf0a4befd4c5a51ece9b0cf45017861
anmol choudhary <78426956+Anmol-choudhary-spec@users.noreply.github.com>
2022-10-27 21:36:20 +0530

    chatting feature updated

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import GetMessages, GetRoomView, SendMsgViewSet, reportView, blockRoom, userPresentOrNot
 
 urlpatterns = [
-    path("checkchater/<str:roomId>/", userPresentOrNot),
+    path("checkchater/<str:roomId>", userPresentOrNot),
     path('getRoom/<str:pk>/', GetRoomView.as_view(), name='getRoom'),
     path('send/<str:roomID>/<str:userID>/', SendMsgViewSet.as_view(), name='send'),
     path('getMessages/<str:room>/', GetMessages.as_view(), name='getMessages'),
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
 from django.http import HttpResponse, JsonResponse
